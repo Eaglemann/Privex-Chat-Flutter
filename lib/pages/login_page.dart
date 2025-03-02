49129c50c1b5c8256d36f9b693d40933a95aca09
Denis Tola <denis.tola22@gmail.com>
2025-03-02 23:49:26 +0100

    Creating Login Page
    -Creating and styling the Login Page.
    -Creating a reusable custom form.

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:privex/widgets/custom_form_field.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: _buildUI(), resizeToAvoidBottomInset: false);
+  }
+
+  Widget _buildUI() {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+        child: Column(
+          children: [_headerText(), _LoginForm(), _createAccountLink()],
+        ),
+      ),
+    );
+  }
+
+  Widget _headerText() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: const Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+
+        children: [
+          Text(
+            'Hi, Welcome Back!',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+          ),
+          Text(
+            'You have been missed!',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  Widget _LoginForm() {
+    return Container(
+      height: MediaQuery.sizeOf(context).height * 0.4,
+      margin: EdgeInsets.symmetric(
+        vertical: MediaQuery.sizeOf(context).height * 0.05,
+      ),
+      child: Form(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomFormField(
+              height: MediaQuery.sizeOf(context).height * 0.1,
+              hintText: "Email",
+            ),
+            CustomFormField(
+              height: MediaQuery.sizeOf(context).height * 0.1,
+              hintText: "Password",
+            ),
+            _loginButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: MaterialButton(
+        onPressed: () {},
+        color: Theme.of(context).colorScheme.primary,
+        child: const Text('Login', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+
+  Widget _createAccountLink() {
+    return const Expanded(
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text("Dont have and account yet?"),
+          Text("Sign Up!", style: TextStyle(fontWeight: FontWeight.w800)),
+        ],
+      ),
+    );
+  }
+}
