be76e3f86cb1fec9c4cc4b664768a931b92b2877
Denis Tola <denis.tola22@gmail.com>
2025-03-03 11:25:57 +0100

    Creating the login class
    -Creating an universal login class
    -Configuring the login class to the main.dart

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  User? _user;
+
+  User? get user {
+    return _user;
+  }
+  AuthService() {}
+
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<bool> login(String email, String password) async {
+    try {
+      final credential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (credential.user != null) {
+        _user = credential.user;
+        return true;
+      }
+    } catch (e) {
+      rethrow;
+    }
+    return false;
+  }
+}
